eec01c88bb2a680f30b56c78cfd7a314056c1436
Anurag Srigyan <51969006+anuajax@users.noreply.github.com>
2024-04-26 00:05:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const errorHandler = require('./errorHandler');
 // app.use(bodyParser.urlencoded({extended: false}));
 // app.use(bodyParser.json());
 
-app.use(cors({origin: ["https://moneytrack-kqzs7dv8f-anurags-projects-14450284.vercel.app",'https://newsapi.org'], credentials: true}));
+app.use(cors({origin: ["https://moneytrack-kqzs7dv8f-anurags-projects-14450284.vercel.app/",'https://newsapi.org'], credentials: true}));
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
