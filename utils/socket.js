69d6aa94f31018cc4c4d7a84fd4684ec073127d8
Anurag Srigyan <hiimanurag122@gmail.com>
2024-04-25 23:59:49 +0530

    remove socket

--- utils/socket.js
+++ utils/socket.js
@@ -1,23 +1,23 @@
-const { Server } = require("socket.io");
-const Notification = require('../models/Notification');
+// const { Server } = require("socket.io");
+// const Notification = require('../models/Notification');
 
-const io = new Server({
-    cors : {
-        origin: "http://localhost:5000",
-        credentials: true
-    }
-});
+// const io = new Server({
+//     cors : {
+//         origin: "http://localhost:5000",
+//         credentials: true
+//     }
+// });
 
-function createNotification(userId, text) {
-    const newNotification = new Notification({
-        userId,
-        text,
-        date: new Date(),
-        read: false
-    });
-    newNotification.save().then(() => {
-        io.to(userId).emit('newNotification', newNotification);
-    }).catch(err => console.error("Failed to save notification", err));
-}
+// function createNotification(userId, text) {
+//     const newNotification = new Notification({
+//         userId,
+//         text,
+//         date: new Date(),
+//         read: false
+//     });
+//     newNotification.save().then(() => {
+//         io.to(userId).emit('newNotification', newNotification);
+//     }).catch(err => console.error("Failed to save notification", err));
+// }
 
-module.exports = {createNotification, io};
+// module.exports = {createNotification, io};
