fc78b22ed80465c69d2871f66261e2f2269a04dc
Anurag Srigyan <hiimanurag122@gmail.com>
2024-04-27 19:28:09 +0530

    fix news api

--- app.js
+++ app.js
@@ -43,8 +43,7 @@ app.get('/api/news', async (req, res) => {
     try {
       const apiKey = 'b511741219fe4f9284ff6bb5f9874e97';
       const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`);
-      console.log(response.data.articles);
-      res.status(200).json(response.articles);
+      res.status(200).json(response);
     } catch (error) {
       res.status(500).json({ message: 'Error fetching news' });
     }
