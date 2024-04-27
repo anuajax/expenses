a0dc42b9a741cc6563579dc99377bf38de3580c4
Anurag Srigyan <hiimanurag122@gmail.com>
2024-04-27 16:46:31 +0530

    src removed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 const passport = require('passport');
-const {schedulerService} = require("../utils/SchedulerService");
+const {schedulerService} = require("./utils/SchedulerService");
 const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
@@ -15,13 +15,13 @@ mongoose.connect(`mongodb+srv://anuajax:${process.env.MONGOPW}@cluster1.j1pcs3e.
 ()=> console.log("DB connected"));
 schedulerService.init();
 
-const diaryRoutes = require('../routes/diaries');
-const itemsRoutes = require('../routes/items');
-const taskRoutes  = require('../routes/tasks');
-const authRoutes = require('../routes/auth');
-const oauthRoutes = require("../middlewares/oAuth");
-const notificationRoutes = require('../routes/notification');
-const errorHandler = require('../errorHandler');
+const diaryRoutes = require('./routes/diaries');
+const itemsRoutes = require('./routes/items');
+const taskRoutes  = require('./routes/tasks');
+const authRoutes = require('./routes/auth');
+const oauthRoutes = require("./middlewares/oAuth");
+const notificationRoutes = require('./routes/notification');
+const errorHandler = require('./errorHandler');
 // app.use(bodyParser.urlencoded({extended: false}));
 // app.use(bodyParser.json());
 
