9190f473ddf1468db5269b139402116a4e136367
Anurag Srigyan <hiimanurag122@gmail.com>
2024-04-28 01:03:17 +0530

    another  news api

--- app.js
+++ app.js
@@ -41,9 +41,8 @@ app.use(errorHandler);
 
 app.get('/api/news', async (req, res) => {
     try {
-      const apiKey = 'b511741219fe4f9284ff6bb5f9874e97';
-      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`);
-      res.status(200).json(response);
+      const response = await axios.get(`http://api.mediastack.com/v1/news?access_key=${process.env.NEWSAPI}&sources=business&limit=40&countries=us`);
+      res.status(200).json(response.data);
     } catch (error) {
       res.status(500).json({ message: 'Error fetching news', error });
     }
